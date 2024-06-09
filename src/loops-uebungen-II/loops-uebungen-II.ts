b640aa080f9918c7e71ca78f9285265be4b01a9d
noctwo <mr.evok@gmail.com>
2024-06-09 13:15:47 +0200

    fizzbuzz

--- src/loops-uebungen-II/loops-uebungen-II.ts
+++ src/loops-uebungen-II/loops-uebungen-II.ts
@@ -53,3 +53,18 @@ function addToFifty(){
 }
 
 addToFifty();
+
+
+//* FIZZ BUZZ
+
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (index % 5 === 0 && index % 3 !== 0){
+        console.log("Buzz");
+    } else if(index % 3 === 0){
+        console.log("Fizz");
+    } else {
+        console.log(index);
+    }
+};
